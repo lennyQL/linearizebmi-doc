4b026dd5a4faa2894d8c2e5d596076cee6c290fa
LennyTGIF <skyzs.9097@gmail.com>
2021-10-17 02:03:07 +0900

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://lennyql.github.io/linearizebmi-doc',
-  baseUrl: '/',
+  url: 'https://lennyql.github.io',
+  baseUrl: '/nearizebmi-doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
