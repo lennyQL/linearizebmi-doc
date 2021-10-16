8868f1c4665dbf508b4ec77e97511be5c482e36e
LennyTGIF <skyzs.9097@gmail.com>
2021-10-17 03:06:14 +0900

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://lennyql.github.io',
-  baseUrl: '/nearizebmi-doc/',
+  baseUrl: '/linearizebmi-doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
